2682590bf74b731c41d734636e1f08cbadf17c04
Branimir Jungic <branimir.jungic@gmail.com>
2018-02-27 22:49:08 +0100

    lista sastojaka se učitava

--- app/src/main/java/hr/math/android/kuharica/ReceptActivity.java
+++ app/src/main/java/hr/math/android/kuharica/ReceptActivity.java
@@ -2,7 +2,12 @@ package hr.math.android.kuharica;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class ReceptActivity extends AppCompatActivity {
 
@@ -10,9 +15,28 @@ public class ReceptActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         String name = savedInstanceState.getString("recipeName");
-        int ID = savedInstanceState.getInt("recipeID");
+        long ID = savedInstanceState.getLong("recipeID");
         TextView name_text = (TextView) findViewById(R.id.recipeName);
         name_text.setText(name);
+        DBRAdapter db = new DBRAdapter(this);
+        db.open();
+        Recept current = db.getReceptZaId(ID);
+        List<String> sastojci = current.getSastojci();
+        List<String> upute = current.getUpute();
+        ListView lista = (ListView) findViewById(R.id.sastojci_list);
+
+        ArrayList<Sastojak> temp = new ArrayList<>();
+        for (String s: sastojci) {
+            Sastojak sas = new Sastojak();
+            sas.setSastojak(s);
+            sas.setSelected(false);
+            temp.add(sas);
+        }
+        SastojciAdapter adapter_sastojci = new SastojciAdapter(this, temp);
+        lista.setAdapter(adapter_sastojci);
+
         setContentView(R.layout.activity_recept);
     }
+
+
 }
