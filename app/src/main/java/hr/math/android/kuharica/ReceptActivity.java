34accf0909a7f8130f014513948af62e75571f0b
Branimir Jungic <branimir.jungic@gmail.com>
2018-02-28 15:18:22 +0100

    lista sastojaka se ispisuje

--- app/src/main/java/hr/math/android/kuharica/ReceptActivity.java
+++ app/src/main/java/hr/math/android/kuharica/ReceptActivity.java
@@ -2,11 +2,15 @@ package hr.math.android.kuharica;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.ListAdapter;
 import android.widget.ListView;
 import android.widget.TextView;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class ReceptActivity extends AppCompatActivity {
@@ -14,19 +18,37 @@ public class ReceptActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        String name = savedInstanceState.getString("recipeName");
-        long ID = savedInstanceState.getLong("recipeID");
+        setContentView(R.layout.activity_recept);
+
+        //String name = savedInstanceState.getString("recipeName");
+        String name = "test_ime";
+        //long ID = savedInstanceState.getLong("recipeID");
+        long ID = 12345;
         TextView name_text = (TextView) findViewById(R.id.recipeName);
         name_text.setText(name);
         DBRAdapter db = new DBRAdapter(this);
         db.open();
+        Recept test_recept = new Recept();
+        test_recept.setId(ID);
+        test_recept.setImeRecepta(name);
+        List<String> temp_list = new ArrayList<String>();
+        temp_list.add("prvi");
+        temp_list.add("drugi");
+        temp_list.add("treci");
+        test_recept.setSastojci(temp_list);
+        test_recept.setNotes("blabla blabla bla");
+        test_recept.setUpute(Arrays.asList("korak 1","korak 2", "korak 3"));
+        test_recept.setPhotoRecept("");
+
+        db.insertRecept(test_recept);
+
         Recept current = db.getReceptZaId(ID);
         List<String> sastojci = current.getSastojci();
         List<String> upute = current.getUpute();
         ListView lista = (ListView) findViewById(R.id.sastojci_list);
 
         ArrayList<Sastojak> temp = new ArrayList<>();
-        for (String s: sastojci) {
+        for (String s: temp_list) {
             Sastojak sas = new Sastojak();
             sas.setSastojak(s);
             sas.setSelected(false);
@@ -34,9 +56,28 @@ public class ReceptActivity extends AppCompatActivity {
         }
         SastojciAdapter adapter_sastojci = new SastojciAdapter(this, temp);
         lista.setAdapter(adapter_sastojci);
+        setListViewHeightBasedOnChildren(lista);
 
-        setContentView(R.layout.activity_recept);
     }
 
+    public static void setListViewHeightBasedOnChildren(ListView listView) {
+        ListAdapter listAdapter = listView.getAdapter();
+        if (listAdapter == null) {
+            // pre-condition
+            return;
+        }
+
+        int totalHeight = 0;
+        for (int i = 0; i < listAdapter.getCount(); i++) {
+            View listItem = listAdapter.getView(i, null, listView);
+            listItem.measure(0, 0);
+            totalHeight += listItem.getMeasuredHeight();
+        }
+
+        ViewGroup.LayoutParams params = listView.getLayoutParams();
+        params.height = totalHeight + (listView.getDividerHeight() * (listAdapter.getCount() - 1));
+        listView.setLayoutParams(params);
+        listView.requestLayout();
+    }
 
 }
